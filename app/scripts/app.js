2fd8a5f3a013095fdc4295db29d5e5c1d2141fc3
董建强 <18513552894@163.com>
2016-09-17 16:09:23 +0800

    后台最初版
    董建强1.0

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name bcXkApp
+ * @description
+ * # bcXkApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('bcXkApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bcXkApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the bcXkApp
+ */
+angular.module('bcXkApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
